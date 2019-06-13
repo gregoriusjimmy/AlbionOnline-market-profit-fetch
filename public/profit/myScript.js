bc9e67ee20408101a97aca3d325796fde16805b0
#45916985 <gregoriusjimmy@gmail.com>
2019-06-13 14:25:17 +0700

    add tier IV and tier V enchant items options

--- public/profit/myScript.js
+++ public/profit/myScript.js
@@ -1,17 +1,50 @@
 const items = [];
-
+const allRegex = [];
+const t4level = [];
+const t5level = [];
 async function getItems() {
    const response = await fetch('items.txt');
    const data = await response.text();
 
+   // selection
+   for (let i = 0; i < 3; i++) {
+      t4level[i] = document.getElementById('t4level' + (i + 1));
+      t5level[i] = document.getElementById('t5level' + (i + 1));
+
+   }
+   console.log(t4level);
+   if (t4level[0].checked) {
+      allRegex.push(/T4_\w+@1/);
+   }
+   if (t4level[1].checked) {
+      allRegex.push(/T4_\w+@2/);
+   }
+   if (t4level[2].checked) {
+      allRegex.push(/T4_\w+@3/);
+   }
+   if (t5level[0].checked) {
+      allRegex.push(/T5_\w+@1/);
+   }
+   if (t5level[1].checked) {
+      allRegex.push(/T5_\w+@2/);
+   }
+   if (t5level[2].checked) {
+      allRegex.push(/T5_\w+@3/);
+   }
+   allRegex.push(/T\w_\w+$/m);
+
+   // console.log(allRegex);
 
    const rows = data.split('\n');
    rows.forEach(element => {
       const item = element.substring(4);
-
-      items.push(item);
+      // console.log(item);
+      for (let i = 0; i < allRegex.length; i++)
+         if (item.match(allRegex[i])) {
+            items.push(item);
+         }
    });
-   // console.log(items);
+   console.log(items);
 }
 
 async function getData() {
@@ -27,6 +60,7 @@ async function getData() {
       const api_url = 'https://www.albion-online-data.com/api/v2/stats/Prices/' + items[i] + '?locations=' + cities_url;
       const response = await fetch(api_url);
       const data = await response.json();
+
       // console.log(data);
       for (let j = 0; j < data.length; j++) {
          if (j == selectedCity) {
